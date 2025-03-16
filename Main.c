a4965919186d2e8ba92f79da9075f0e8533c0e3c
Alan Bui <134470174+alan-huy-bui@users.noreply.github.com>
2025-03-16 18:50:57 -0500

    Main.c: Update 1

--- Main.c
+++ Main.c
@@ -2,10 +2,13 @@
 #include "Computer.h"
 #include "Assembler.h"
 
+
 int main() {
 
+    //  Assemble Program
     unsigned short int* programPtr = assemble(0, 0);
 
+    //  Run Program
     initializeComputer(programPtr, 6);
 
     return 0;
